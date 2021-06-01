7d08bb879596278b73d33964896bd678fdafe183
Bek <kruzus@live.com>
2021-06-01 05:58:15 -0400

    lol

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/")
-def home():
-    return "Hello"
 
 
-app.run()
+
+
+class Home():
+
